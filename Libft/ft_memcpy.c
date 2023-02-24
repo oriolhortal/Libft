3d97103a2319083f585c7dfff913275b4e963006
Oriol <119789057+oriolhortal@users.noreply.github.com>
2023-02-24 12:39:59 +0100

    Add files via upload

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ohortal- <ohortal-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 11:44:40 by ohortal-          #+#    #+#             */
-/*   Updated: 2023/01/25 15:44:01 by ohortal-         ###   ########.fr       */
+/*   Updated: 2023/02/23 12:37:08 by ohortal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -21,7 +21,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		return (dest);
 	i = 0;
 	s = (unsigned char *)src;
-	d = (unsigned char *) dest;
+	d = (unsigned char *)dest;
 	while (i < n)
 	{
 		d[i] = s[i];
--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,33 +6,33 @@
 /*   By: ohortal- <ohortal-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 14:39:03 by ohortal-          #+#    #+#             */
-/*   Updated: 2023/02/09 15:02:08 by ohortal-         ###   ########.fr       */
+/*   Updated: 2023/02/23 12:03:58 by ohortal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-static int	ft_count_word(char const *s, char c)
+static int	ft_count_substrings(char const *s, char c)
 {
 	int	i;
-	int	word;
+	int	substrs;
 
 	i = 0;
-	word = 0;
+	substrs = 0;
 	while (s && s[i])
 	{
 		if (s[i] != c)
 		{
-			word++;
+			substrs++;
 			while (s[i] != c && s[i])
 				i++;
 		}
 		else
 			i++;
 	}
-	return (word);
+	return (substrs);
 }
 
-static int	ft_size_word(char const *s, char c, int i)
+static int	ft_size_substring(char const *s, char c, int i)
 {
 	int	size;
 
@@ -55,27 +55,27 @@ static void	ft_str_free(char **strs, int j)
 char	**ft_split(char const *s, char c)
 {
 	int		i;
-	int		word;
+	int		substrs;
 	char	**strs;
 	int		j;
 
 	i = 0;
 	j = -1;
-	word = ft_count_word(s, c);
-	strs = (char **)malloc((word + 1) * sizeof(char *));
+	substrs = ft_count_substrings(s, c);
+	strs = (char **)malloc((substrs + 1) * sizeof(char *));
 	if (!(strs))
 		return (NULL);
-	while (++j < word)
+	while (++j < substrs)
 	{
 		while (s[i] == c)
 			i++;
-		strs[j] = ft_substr(s, i, ft_size_word(s, c, i));
+		strs[j] = ft_substr(s, i, ft_size_substring(s, c, i));
 		if (!(strs[j]))
 		{
 			ft_str_free(strs, j);
 			return (NULL);
 		}
-		i += ft_size_word(s, c, i);
+		i += ft_size_substring(s, c, i);
 	}
 	strs[j] = 0;
 	return (strs);
